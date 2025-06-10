5830b8082fa497f5b7e6d8202546f8397321bf77
Mobb autofixer <git@mobb.ai>
2025-06-10 04:39:32 +0000

    Zip Slip fix by mobb-a48cb77f-282e-43dc-b243-f27979e4e3fe

--- zip_slip_csharp_codeql_sample/program.cs
+++ zip_slip_csharp_codeql_sample/program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.IO.Compression;
+using System.IO;
 
 public class Program
 {
@@ -19,13 +20,36 @@ public class Program
     {
         foreach (var entry in archive.Entries)
         {
-            string file = entry.FullName;
+            string file = GetRelativePath(entry.FullName);
             if (!string.IsNullOrEmpty(file))
             {
                 string destFileName = Path.Combine(destDirectory, file);
                 entry.ExtractToFile(destFileName, true);
 
             }
+        }
+    }
+
+    public static string GetRelativePath(string path)
+    {
+        string basePath;
+        string filePath;
+        try
+        {
+            basePath = Path.GetFullPath(".") + Path.DirectorySeparatorChar;
+            filePath = Path.GetFullPath(path);
+        }
+        catch (IOException e)
+        {
+            throw new ArgumentException("Potential directory traversal attempt", e);
+        }
+        if (filePath.StartsWith(basePath))
+        {
+            return filePath;
+        }
+        else
+        {
+            throw new ArgumentException("Potential directory traversal attempt");
         }
     }
 }
